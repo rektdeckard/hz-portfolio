b205051deb400b1e1f6377fc4ee32a0b44dd5c1f
rektdeckard <friedtm@gmail.com>
2020-07-10 02:50:35 -0400

    Add clarifying comments to 'static.js'

--- js/static.js
+++ js/static.js
@@ -1,24 +1,29 @@
-let canvas = document.getElementById("static");
-let context = canvas.getContext("2d");
-let { offsetHeight, offsetWidth } = canvas;
-canvas.width = offsetWidth;
-canvas.height = offsetHeight;
-let pixelSize = 1;
+const canvas = document.getElementById("static");
 
-let idata = context.createImageData(offsetWidth, offsetHeight);
-let buffer = new Uint32Array(idata.data.buffer);
+if (canvas) {
+  const context = canvas.getContext("2d");
+  const { offsetHeight, offsetWidth } = canvas;
+  canvas.width = offsetWidth;
+  canvas.height = offsetHeight;
+  const pixelSize = 1;
 
-// (function loop() {
-//   noise(context);
-//   requestAnimationFrame(loop);
-// })();
+  const idata = context.createImageData(offsetWidth, offsetHeight);
+  const buffer = new Uint32Array(idata.data.buffer);
 
-function noise(context) {
-  let len = buffer.length - 1;
-  while (len--) {
-    buffer[len] = Math.random() < 0.5 ? 0 : -1 >> 0;
-  }
-  context.putImageData(idata, 0, 0);
+  function noise(context) {
+    let len = buffer.length - 1;
+    while (len--) {
+      buffer[len] = Math.random() < 0.5 ? 0 : -1 >> 0;
+    };
+    context.putImageData(idata, 0, 0);
+  };
+
+  // This method will generate static at the fastest possible render speed
+  // (function loop() {
+  //     noise(context);
+  //     requestAnimationFrame(loop);
+  // })();
+
+  // This method will generate static at a fixed interval
+  setInterval(() => noise(context), 24); // in milliseconds
 };
-
-setInterval(() => noise(context), 24);
