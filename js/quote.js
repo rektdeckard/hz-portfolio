4d398c3645d45fad4b1e81a9129043242c3ff64d
rektdeckard <friedtm@gmail.com>
2021-02-27 18:35:49 -0500

    quote: Cycle every 5 seconds

--- js/quote.js
+++ js/quote.js
@@ -1,17 +1,22 @@
 // Add single-quoted phrases or quotations here, separated
 // by commas, to be randomly shown on page load
 const quotes = [
-// Ideal length ~= 30 characters OR 1 emoji + 27 characters
-  '👩🏻‍💻 Beep boop boop',
-  '🎶 Listening: Summer Walker',
-  '🎵 Listening: SZA',
-  '🎶 Listening: Lúnasa',
-  '📺 Watching: Halt and Catch Fire',
-  '📺 Watching: Disenchantment',
-  '🎮 Playing: Hades',
+  // Ideal length ~= 30 characters OR 1 emoji + 27 characters
+  "👩🏻‍💻 Beep boop boop",
+  "🎶 Listening: Summer Walker",
+  "🎵 Listening: SZA",
+  "🎶 Listening: Lúnasa",
+  "📺 Watching: Halt and Catch Fire",
+  "📺 Watching: Disenchantment",
+  "🎮 Playing: Hades",
 ];
 
-const quoteSpan = document.getElementById("quote");
-if (quoteSpan) {
-  quoteSpan.innerHTML = quotes[Math.floor(Math.random() * quotes.length)];
-};
+function randomQuote() {
+  const quoteSpan = document.getElementById("quote");
+  if (quoteSpan) {
+    quoteSpan.innerHTML = quotes[Math.floor(Math.random() * quotes.length)];
+  }
+}
+
+randomQuote();
+window.setInterval(randomQuote, 5000);
